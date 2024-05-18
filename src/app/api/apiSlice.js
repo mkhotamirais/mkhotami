61536ed61a67569c1a6efef2ca8f15f6d3943d93
Mkhotamirais <mkhotamirais@gmail.com>
2024-05-18 14:41:58 +0700

    lagi

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
+    baseUrl: "https://mkhotami-server.vercel.app/api",
+    // baseUrl: "/api",
     credentials: "include",
   }),
   tagTypes: ["User", "Category", "Tag", "Product"],
